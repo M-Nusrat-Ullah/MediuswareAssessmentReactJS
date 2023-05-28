23ea37b00ab266f2b573dad2a870e770a1b48a7a
M-Nusrat-Ullah <nusrat15-10498@diu.edu.bd>
2023-05-28 19:49:16 +0600

    solved problem1

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -3,6 +3,48 @@ import React, {useState} from 'react';
 const Problem1 = () => {
 
     const [show, setShow] = useState('all');
+    const [name, setName] = useState();
+    const [status, setStatus] = useState();
+    const [data, setData] = useState([]);
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    
+        const newData = { name, status };
+        setData(prevdata => [...prevdata, newData]);
+    
+        // Reset the form fields
+        setName('');
+        setStatus('');
+
+    };
+
+    const filteredData = data.filter((item) => {
+        if (show === 'all') {
+          return true;
+        } else if (show === 'active') {
+          return item.status === 'active';
+        } else if (show === 'completed') {
+          return item.status === 'completed';
+        }
+        return false;
+      }).sort((a, b) => {
+        if (a.status === 'active' && b.status !== 'active') {
+          return -1;
+        } else if (a.status === 'completed' && b.status !== 'active' && b.status !== 'completed') {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+
+    const renderedData = filteredData.map((item,index) => (
+        <tr key={index}>
+            <td>{item.name}</td>
+            <td>{item.status}</td>
+        </tr>
+    ))
 
     const handleClick = (val) =>{
         setShow(val);
@@ -14,12 +56,12 @@ const Problem1 = () => {
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form className="row gy-2 gx-3 align-items-center mb-4" onSubmit={handleSubmit}>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input type="text" className="form-control" value={name} placeholder="Name" onChange={(e) => setName(e.target.value)}/>
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input type="text" className="form-control" value={status} placeholder="Status" onChange={(e) => setStatus(e.target.value)}/>
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary">Submit</button>
@@ -47,7 +89,7 @@ const Problem1 = () => {
                         </tr>
                         </thead>
                         <tbody>
-                        
+                            {renderedData}
                         </tbody>
                     </table>
                 </div>
